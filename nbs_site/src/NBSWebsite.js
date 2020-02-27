3c524aa70460e519acd0508caa4142cfdf9bcdac
Joefatawesome <jcavish@gmail.com>
2020-02-26 21:44:08 -0800

    -- Adding basic components to page. Header, footer, and homepage.

--- nbs_site/src/NBSWebsite.js
+++ nbs_site/src/NBSWebsite.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import logo from './images_icons/nbs_icon.ico'
 import COMPANY_NAME from './common/StringConstants.js';
+import Header from './Components/HeaderCompent/header';
+import Footer from "./Components/FooterComponent/footer";
+import HomePage from "./Components/Pages/homePage";
 import './Assets/css/default.min.css';
 
 class NBSWebsite extends Component {
     render() {
         return (
             <div className="NBSWebsite">
-
+                <Header />
+                    <HomePage />
+                <Footer />
             </div>
         );
     }
