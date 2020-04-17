5b7b99dfc92819122a47434a9890cba11c73090d
Joefatawesome <jcavish@gmail.com>
2020-04-16 23:19:45 -0700

    -- News, Client Forms, About Us, and Contact Us pages are stubbed out and are linked to from the navigation panel.

--- nbs_site/src/NBSWebsite.js
+++ nbs_site/src/NBSWebsite.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import {
     BrowserRouter as Router,
-    Route,
-    Link
+    Route
 } from 'react-router-dom';
 
 import Header from './Components/HeaderComponent/header';
 import Footer from "./Components/FooterComponent/footer";
 import HomePage from "./Components/Pages/homePage";
-import Products from "./Components/Pages/products";
+import News from "./Components/Pages/newsPage";
+import ClientForms from "./Components/Pages/clientForms";
+import AboutUs from "./Components/Pages/aboutUs";
+import ContactInfo from "./Components/Pages/contactInfo";
 
 import './Assets/css/default.min.css';
 
@@ -19,8 +21,10 @@ class NBSWebsite extends Component {
                 <div className="NBSWebsite">
                     <Header/>
                         <Route exact path='/' component={HomePage}/>
-                        <Route exact path='/Products' component={Products}/>
-
+                        <Route exact path='/News' component={News}/>
+                        <Route exact path='/Forms' component={ClientForms}/>
+                        <Route exact path='/About' component={AboutUs}/>
+                        <Route exact path='/Contact' component={ContactInfo}/>
                     <Footer/>
                 </div>
             </Router>
