ea684a293c924f037fc7168f8aa9371df9a64e49
Joefatawesome <jcavish@gmail.com>
2020-04-25 12:31:38 -0700

    Updates

---
+++ precache-manifest.c9559236fac5d0f0b9b6016e6bf9e1f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccf5427ca4a8242f15aee2d27d37afd",
+    "url": "/react_training/index.html"
+  },
+  {
+    "revision": "3ed3af9b67a58f828dd6",
+    "url": "/react_training/static/css/main.f039cfb6.chunk.css"
+  },
+  {
+    "revision": "77379850b1ca5b07514a",
+    "url": "/react_training/static/js/2.3cc17584.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react_training/static/js/2.3cc17584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed3af9b67a58f828dd6",
+    "url": "/react_training/static/js/main.91ddc945.chunk.js"
+  },
+  {
+    "revision": "ca132ae50a9e7421919d",
+    "url": "/react_training/static/js/runtime-main.4f199055.js"
+  },
+  {
+    "revision": "b0c4f911772adf6a50bf7213e50c4750",
+    "url": "/react_training/static/media/rectangles_icon_final.b0c4f911.png"
+  }
+]);
