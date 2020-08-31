6b7817df3ab361de4ab9b500cf7df25817a3ea6b
Joefatawesome <jcavish@gmail.com>
2020-08-30 22:04:39 -0700

    -- Added a drop down for navigation bar and aligned it to the right.

--- nbs_site/src/Components/HeaderComponent/header.js
+++ nbs_site/src/Components/HeaderComponent/header.js
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom';
 
 import nbs_icon from '../../imagesIcons/rectangles_icon_final.png';
+import $darkGrey from '../../Assets/scss/config.scss'
 
 let img;
 let canvas;
@@ -20,6 +21,7 @@ class Header extends Component {
         canvas = null;
         this.setCanvasRef = element => {
             canvas = element;
+            //TODO: trying to position drop down before working on making the button work
         };
 
     }
@@ -36,17 +38,18 @@ class Header extends Component {
             //dWidth/dHeight is the width and height of the displayed imaged
             //ctx.drawImage(img, 0, 0, MaxLength, MaxHeight, left margin, top margin, 50, 50);
         };
-        ctx.font = "48px Sans-Serif";
-        ctx.fillStyle = "white";
-        ctx.fillText("Norwalk Business Service", 100, 65);
+        ctx.font = "24px Sans-Serif";
+        ctx.fillStyle = $darkGrey;
+        ctx.fillText("Norwalk Business Service", 100, 52);
     }
     //TODO: Modernize here.
     render() {
         return (
             <header>
                 <canvas ref={this.setCanvasRef} width={900} height={90}/>
-                <nav>
-                    <ul>
+                <div className="dropdown" style={{float: "right"}}>
+                    <button onClick="myFunction()" className="dropbtn">Dropdown</button>
+                    <div id="myDropdown" className="dropdown-content">
                         <li className="first">
                             <Link to='/'>Home</Link>
                         </li>
@@ -62,11 +65,49 @@ class Header extends Component {
                         <li className="last">
                             <Link to='/Contact'>Contact Info</Link>
                         </li>
-                    </ul>
-                </nav>
+                    </div>
+                </div>
             </header>
         );
     }
+
+/*    /!* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content *!/
+    myFunction() =>{
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+// Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }*/
+
 }
+/*<ul>
+    <li className="first">
+        <Link to='/'>Home</Link>
+    </li>
+    <li>
+        <Link to='/News'>News</Link>
+    </li>
+    <li>
+        <Link to={'/Forms'}>Client Forms</Link>
+    </li>
+    <li>
+        <Link to={'/About'}>About Us</Link>
+    </li>
+    <li className="last">
+        <Link to='/Contact'>Contact Info</Link>
+    </li>
+</ul>*/
 
 export default Header;
